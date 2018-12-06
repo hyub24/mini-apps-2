7a0457c0969bd4daf4292c6cfda10e8261f06d9f
in hyub jeon <inhyub@inhyubs-MacBook-Air.local>
2018-12-06 14:46:30 -0800

    uses redis to cache

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
+const redis = require('redis');
+
+const client = redis.createClient();
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/crypto/history/:start/:end', (req, res) => {
-  if(req.params.start === '0') {
-    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
-      .then(response => res.send(response.data))
-      .catch(error => res.status(500).send(error));
-  } else {
-    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${req.params.start}&end=${req.params.end}`)
-      .then(response => res.send(response.data))
-      .catch(error => res.status(500).send(error));
-  }
+  const start = req.params.start;
+  const end = req.params.end;
+  client.get(`${start}${end}`, (err, reply) => {
+    if(err || !reply) {
+      if(start === '0') {
+        axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+          .then(response => {
+            client.setex(`${start}${end}`, 3600, JSON.stringify(response.data));
+            res.send(response.data);
+          })
+          .catch(error => res.status(500).send(error));
+      } else {
+        axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
+          .then(response => {
+            client.setex(`${start}${end}`, 3600, JSON.stringify(response.data));
+            res.send(response.data);
+          })
+          .catch(error => res.status(500).send(error));
+      }
+    } else {
+      res.send(reply);
+    }
+  })
+
 
 });
 
