a4a9afa50bc25b62a0b0670b8f88c480f7a7ae30
in hyub jeon <inhyub@inhyubs-MacBook-Air.local>
2018-12-06 11:07:08 -0800

    set up webpack for cryptochart

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,16 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
 
+class App extends React.Component {
+  constructor() {
+
+  }
+  render() {
+    return(
+      <div>hi</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
