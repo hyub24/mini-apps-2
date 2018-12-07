dba6b7e0074c8d2dbe90ff662e0c6173f4d41461
in hyub jeon <inhyub@inhyubs-MacBook-Air.local>
2018-12-07 11:22:06 -0800

    added webpack

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>hi</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
